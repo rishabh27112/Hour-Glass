380c2edbb3bc1e0fd93332fe04ce18dd80ce42e5
Nakul Patel <nakuljpatel1@gmail.com>
2025-09-24 06:32:44 +0530

    Made home page responsive

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './HomePage.module.css';
-import logoIcon from '../Logo/main_logo.png';
 
 const HomePage = () => {
   return (
