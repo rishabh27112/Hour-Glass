cc74e8fb7f42f3bf059ebb33a4dbe635134fc07a
Nakul Patel <nakuljpatel1@gmail.com>
2025-11-01 15:30:44 +0530

    Added SignUpPage and ForgotPasswordPage

--- Frontend/UI/src/App.jsx
+++ Frontend/UI/src/App.jsx
@@ -4,12 +4,16 @@ import { Routes, Route } from 'react-router-dom';
 // import pages
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage'
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage  />} />
       <Route path="/signin" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
     </Routes>
   )
 }
