f38ae4247f3c4edc0b8e912b96a1f9ccd9e5ac71
Nakul Patel <nakuljpatel1@gmail.com>
2025-11-01 15:32:35 +0530

    Added page gor main button

---
+++ Frontend/UI/src/components/MainButton.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const MainButton = (props) => {
+    return (
+        <button
+            type="submit"
+            className="
+    group
+    relative overflow-hidden 
+    w-full bg-[#18d4d1] text-[#131212] font-bold py-2 px-5 rounded-lg
+    tracking-light transition-all duration-300 ease-in-out 
+    hover:translate-y-0.5
+  ">
+
+            <span className="
+                absolute inset-0 
+                -translate-x-full 
+                -skew-x-12 
+                bg-linear-to-r from-transparent via-white to-transparent
+                transition-transform duration-500 ease-in-out
+                group-hover:translate-x-full 
+            ">
+            </span>
+            <span className="relative z-10">
+              {props.txt}
+            </span>
+        </button>
+    )
+}
+
+export default MainButton
