79e7278fb62621ef9f2668e8a370bf3c12b1123a
Nakul Patel <nakuljpatel1@gmail.com>
2025-11-01 15:33:13 +0530

    Made some animations in logo

---
+++ Frontend/UI/src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div className="group inline-block">
+            <i className="ri-hourglass-line text-[#18d4d1] text-6xl 
+                          inline-block
+                          transition-transform duration-500 ease-in-out
+                          group-hover:rotate-180 "></i>
+        </div>
+    )
+}
+
+export default Logo
